7d523a4318033119c240d5655545d532c355e177
Myat-MinKhant <myatminkhant.hee@gmail.com>
2023-07-27 20:49:41 +0630

    First Commit

---
+++ index.js
@@ -0,0 +1,115 @@
+const apiKey = "bb54f7b2b7df1fb3997388d5d115ba52";
+const imgApi = "https://image.tmdb.org/t/p/w1280";
+const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
+const form = document.querySelector("#search-form");
+const query = document.querySelector("#search-input");
+const result = document.querySelector("#result");
+
+let page = 1;
+let isSearching = false;
+
+async function fetchData(url) {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    return await res.json();
+  } catch (error) {
+    return null;
+  }
+}
+
+async function fetchAndShowResult(url) {
+  const data = await fetchData(url);
+  if (data && data.results) {
+    showResults(data.results);
+  }
+}
+
+function createMovieCard(movie) {
+  const { poster_path, original_title, release_date, overview } = movie;
+  const imagePath = poster_path ? imgApi + poster_path : "";
+  const truncatedTitle =
+    original_title.length > 15
+      ? original_title.slice(0, 15) + "..."
+      : original_title;
+  const formattedDate = release_date || "No release date";
+  const cardTemplate = `
+        <div class='column'>
+            <div class="card">
+                <a href="#" class="card">
+                    <img src='${imagePath}' alt='${original_title}' width='100%' />
+                </a>
+                <div class="card-content">
+                    <div class="card-header">
+                        <div class="left-content">
+                            <h3 style='font-weight: 600'>${truncatedTitle}</h3>
+                            <span style='color: #12efec'>${formattedDate}</span>
+                        </div>
+                        <div class="right-content">
+                          <a href="${imagePath}" target="_blank" class='card-btn'>See Cover</a>  
+                        </div>
+                    </div>
+                    <div class="info">
+                        ${overview || "No overview yet..."}
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+  return cardTemplate;
+}
+
+function clearResults() {
+  result.innerHTML = "";
+}
+
+function showResults(item) {
+  const newContent = item.map(createMovieCard).join("");
+  result.innerHTML += newContent || "<p>No results found.</p>";
+}
+
+async function loadMoreResult() {
+  if (isSearching) {
+    return;
+  }
+  page++;
+  const searchTerm = query.value;
+  const url = searchTerm
+    ? `${searchUrl}${searchTerm}&page=${page}`
+    : `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
+  await fetchAndShowResult(url);
+}
+
+function detectEnd() {
+  const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 20) {
+    loadMoreResult();
+  }
+}
+
+async function handleSearch(e) {
+  e.preventDefault();
+  const searchTerm = query.value.trim();
+  if (searchTerm) {
+    isSearching = true;
+    clearResults();
+    const newUrl = `${searchUrl}${searchTerm}&page=${page}`;
+    await fetchAndShowResult(newUrl);
+    query.vlaue = "";
+  }
+}
+
+form.addEventListener("submit", handleSearch);
+window.addEventListener("scroll", detectEnd);
+window.addEventListener("resize", detectEnd);
+
+async function init() {
+  clearResults();
+  const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
+  isSearching = false;
+  await fetchAndShowResult(url);
+}
+
+init();
